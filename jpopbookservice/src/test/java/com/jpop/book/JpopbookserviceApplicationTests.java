8d72204e015395b48f30736b681618348ac220f5
Ravi Tripathi <Ravi_Tripathi@epam.com>
2021-01-06 18:28:57 +0530

    spring boot app added

---
+++ jpopbookservice/src/test/java/com/jpop/book/JpopbookserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpop.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpopbookserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
